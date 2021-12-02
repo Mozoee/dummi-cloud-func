f7a1f177c8954b79d6e934cbf8ba17af223968c8
Muhammad Maouz Qazi <pk.qazi92@gmail.com>
2021-12-02 14:58:44 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 exports.helloPubSub = (message, context) => {
+  console.log('projectId: ',process.env.projectId);
+  console.log('subscriptionName: ',process.env.subscriptionName);
+  console.log('credentials: ',process.env.credentials);
+  
+ 
   const name = message.data
     ? Buffer.from(message.data, 'base64').toString()
     : 'World';
