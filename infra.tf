fafad66580a6c4228482392a28677b5e2b701c7a
Mozoee <pk.qazi92@gmail.com>
2021-12-08 10:45:36 +1100

    rm oauth from sched

--- infra.tf
+++ infra.tf
@@ -46,10 +46,6 @@ resource "google_cloud_scheduler_job" "job" {
   http_target {
     http_method = "GET"
     uri         = "${ var.funcurl }"
-
-    oauth_token {
-      service_account_email = "cs-ps-cf-scheduler@fifth-sunup-329021.iam.gserviceaccount.com"
-    }
   }
 
   
